cf62f24759b788bb10408cfb1444a2b0f1033e04
Nathaniel Tucker <nathaniel.tucker@coinbase.com>
2019-05-22 13:34:12 -0700

    Ensure return value of HOC extends Object

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import hoistStatics from 'hoist-non-react-statics';
 
 type Diff<T, U> = T extends U ? never : T;
 
-export default function withHooks<T, P>(use: (props: P) => T) {
+export default function withHooks<T extends object, P extends object>(use: (props: P) => T) {
   return <Props extends T & P>(Component: React.ComponentClass<Props>) => {
     const WrappedComponent = hoistStatics((props: Diff<Props, T>) => {
       const hookProps = use(props);
