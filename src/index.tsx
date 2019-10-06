96bc517de3f601cb30aad5203126301bc1b68396
Nathaniel Tucker <me@ntucker.me>
2019-10-06 14:17:48 -0700

    [feature] Forward refs and add displayName

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,11 @@ type Diff<T, U> = T extends U ? never : T;
 
 export default function withHooks<T extends object, P extends object>(use: (props: P) => T) {
   return <Props extends T & P>(Component: React.ComponentClass<Props>) => {
-    const WrappedComponent = hoistStatics((props: Diff<Props, T>) => {
+    const WrappedComponent = hoistStatics(React.forwardRef((props: Diff<Props, T>, ref) => {
       const hookProps = use(props);
-      return <Component {...props} {...hookProps} />
-    }, Component);
+      return <Component {...props} {...hookProps} ref={ref} />
+    }), Component);
+    WrappedComponent.displayName = `withHook(${use.name})(${Component.displayName || Component.name})`
     return WrappedComponent;
   }
 }
