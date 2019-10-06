ca31dfeab2c748a5af761e151c06d6034f49f8f5
Nathaniel Tucker <me@ntucker.me>
2019-10-06 14:31:52 -0700

    [internal] Update linting

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import hoistStatics from 'hoist-non-react-statics';
 
 type Diff<T, U> = T extends U ? never : T;
 
-export default function withHooks<T extends object, P extends object>(use: (props: P) => T) {
+export default function withHooks<T extends object, P extends object>(
+  use: (props: P) => T,
+) {
   return <Props extends T & P>(Component: React.ComponentClass<Props>) => {
-    const WrappedComponent = hoistStatics(React.forwardRef((props: Diff<Props, T>, ref) => {
-      const hookProps = use(props);
-      return <Component {...props} {...hookProps} ref={ref} />
-    }), Component);
-    WrappedComponent.displayName = `withHook(${use.name})(${Component.displayName || Component.name})`
+    const WrappedComponent = hoistStatics(
+      React.forwardRef((props: Diff<Props, T>, ref) => {
+        const hookProps = use(props);
+        return <Component {...props} {...hookProps} ref={ref} />;
+      }),
+      Component,
+    );
+    WrappedComponent.displayName = `withHook(${
+      use.name
+    })(${Component.displayName || Component.name})`;
     return WrappedComponent;
-  }
+  };
 }
