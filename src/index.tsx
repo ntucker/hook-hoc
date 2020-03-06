a06cded684c8c968cf1f00687f20a6e9e115b48c
Nathaniel Tucker <me@ntucker.me>
2020-03-06 10:46:25 -0800

    fix: Correctly infer prop types for wrapped component

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import hoistStatics from 'hoist-non-react-statics';
 
-type Diff<T, U> = T extends U ? never : T;
-
-export default function withHooks<T extends object, P extends object>(
-  use: (props: P) => T,
-) {
+export default function withHooks<
+  T extends Record<string, any>,
+  P extends Record<string, any>
+>(use: (props: P) => T) {
   return <Props extends T & P>(
     Component: React.JSXElementConstructor<Props> & React.ComponentType<Props>,
   ) => {
     const WrappedComponent = hoistStatics(
       // eslint-disable-next-line react/display-name
-      React.forwardRef((props: Diff<Props, T>, ref) => {
-        const hookProps = use(props);
-        return <Component {...props} {...hookProps} ref={ref} />;
+      React.forwardRef((props: Omit<Props, keyof T>, ref) => {
+        const hookProps = use(props as any);
+        return <Component {...(props as any)} {...hookProps} ref={ref} />;
       }),
       Component,
     );
